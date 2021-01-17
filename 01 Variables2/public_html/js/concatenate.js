ee826670a9e0970599b3f45afb98ba6b6566b886
Moctader <54657677+Moctader@users.noreply.github.com>
2021-01-17 13:26:05 +0200

    Add files via upload

---
+++ 01 Variables2/public_html/js/concatenate.js
@@ -0,0 +1,36 @@
+/* 
+    Created on : May, 2019 
+    Author     : Liisa Auer, Oulu University of Applied Sciences
+    Licence    : CC-BY-4.0
+*/
+
+/*
+ * Student name :
+ * Started date :
+ * Ended   date :
+ * 
+ */
+
+function j01() {
+
+}
+
+function j02() {
+
+}
+
+function j03() {
+
+}
+
+function j04() {
+    const START = "Hello ";
+    const MIDDLE = ", you are ";
+    const END = " years of age.";
+    
+}
+
+function j05() {
+
+}
+
