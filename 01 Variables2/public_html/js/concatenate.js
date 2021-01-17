21ca9b592b1547364754c6a51a2b2621e7987bb2
Moctader <moctader.golam@gmail.com>
2021-01-17 14:52:07 +0200

    new

--- 01 Variables2/public_html/js/concatenate.js
+++ 01 Variables2/public_html/js/concatenate.js
@@ -12,6 +12,23 @@
  */
 
 function j01() {
+    // read input data
+    let year1 = Number(document.getElementById("year1").value);
+    let year2 = Number(document.getElementById("year2").value);
+
+    let today = new Date(); // current date
+    let current_year = today.getFullYear();
+
+    //calculations
+    let age1 = current_year - year1;
+    let age2 = current_year - year2;
+    let difference = age1 - age2
+
+    // print
+    document.getElementById("age1").innerHTML="Person 1 age is " + age1;
+    document.getElementById("age2").innerHTML="Person 2 age is " + age2;
+    document.getElementById("difference").innerHTML="Person 1 is " + difference +" years older than person 2";
+
 
 }
 
